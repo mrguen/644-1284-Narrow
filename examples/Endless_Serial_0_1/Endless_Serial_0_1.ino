b147e9d9e764322e4ba5b591e46f494fc02ec426
Thierry GUENNOU <t_guennou@hotmail.com>
2020-12-07 14:20:20 +0100

    Update Endless_Serial_0_1.ino

--- examples/Endless_Serial_0_1/Endless_Serial_0_1.ino
+++ examples/Endless_Serial_0_1/Endless_Serial_0_1.ino
@@ -1,20 +1,14 @@
 /*
-  Multiple Serial test
+  Infinite loop serial test
 
-  Receives from the main serial port, sends to the others.
+  Receives from the main serial port and sends back. The Tw signal is fed also to Serial 1 Rx.
   Receives from serial port 1, sends to the main serial (Serial 0).
 
   This example works only with boards with more than one serial like Arduino Mega, Due, Zero etc.
 
   The circuit:
-  - any serial device attached to Serial port 1
+  - A cable connected betwwen "TX" and "10" pins
   - Serial Monitor open on Serial port 0
-
-  created 30 Dec 2008
-  modified 20 May 2012
-  by Tom Igoe & Jed Roach
-  modified 27 Nov 2015
-  by Arturo Guadalupi
 
   This example code is in the public domain.
 */
